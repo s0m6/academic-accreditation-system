34ee385bfa5c15195c8b32d2084462c3dc8b63ef
sam alyafei <sam6dev@gmail.com>
2026-06-21 18:23:19 +0300

    change char from CDN to local

--- resources/js/app.js
+++ resources/js/app.js
@@ -7,10 +7,12 @@ import Alpine from 'alpinejs';
 import SignaturePad from 'signature_pad';
 
 import Swal from 'sweetalert2';
+import Chart from 'chart.js/auto';
 
 // Expose globals for use in Blade templates
 window.SignaturePad = SignaturePad;
 window.Swal = Swal;
+window.Chart = Chart;
 
 window.Alpine = Alpine;
 
--- resources/views/council_secretariat/reports/index.blade.php
+++ resources/views/council_secretariat/reports/index.blade.php
@@ -490,7 +490,6 @@
 </div>
 
 @push('scripts')
-<script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script>
     document.addEventListener('DOMContentLoaded', () => {
         // 1. Stages Chart
